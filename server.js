ba40f62b53e33b11ec54cf811e3557bfa3854416
avik067 <avikbiswas067@gmail.com>
2023-09-09 15:33:59 +0530

    modification , primary starter part is replaced

--- server.js
+++ server.js
@@ -1,16 +1,66 @@
+require('dotenv').config()
 const express = require('express')
+const app = express() 
 const cors = require('cors')
-const app = express()
+const path = require ('path')
+
+
+const  port  =  process.env.PORT || 4000 ;
+
+
+const http = require('http').Server(app) ///// 
+
 
 app.use(cors())
+
 app.use(express.json())
 
+////////////////////////////// http server + -> socket.io
+
+const io = require ('socket.io')(http,{
+  cors: {
+    origin: "*"
+  }
+}) ;
+
+
+
+///////////////////////////////// listening to app 
+// app.listen(port,()=> {  // we wont be listening to the app directly , we will now listen via io inside which we have create socket server 
+//   console.log(`Node is running : ${port}`) ;
+// })
+////////////////////////////////// Home route
 
 app.get('/', function (req, res) {
   res.send('A small backend application which can generate and emit an encrypted data stream over a socket !!! Live 😃 ')
 })
 
-app.listen(3000,()=> {
+//////////////////////////////// connection
 
-    console.log("Node is running : 3000") ;
+io.on('connection', socket => {
+      //  console.log(socket)
+      // console.log(socket.id)
+        console.log("a user is connected")
+
+      socket.on('disconnect',()=> {
+        console.log("user disconnected")
+      })
+      
+      socket.on("new-message" ,(msg)=> {
+        console.log(`from client : ${msg}`)
+      })
+     
+      socket.emit("message",(msg)=>{`Received by the server again ${msg}`})
+       
+      socket.emit("server","Received by the server")
+
+    })
+
+/////////////////////////
+
+
+
+http.listen(port,()=> {
+  console.log(`Node is running : ${port}`) ;
 })
+
